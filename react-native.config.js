c7b78c401e72a2fe2f5659a797eeb7ff75aae260
William Swanson <swansontec@gmail.com>
2024-04-18 16:26:03 -0700

    Enable the new architecture

--- react-native.config.js
+++ react-native.config.js
@@ -5,5 +5,17 @@ module.exports = {
         ios: null
       }
     }
+  },
+  project: {
+    android: {
+      unstable_reactLegacyComponentNames: [
+        // list of conponents that needs to be wrapped by the interop layer
+      ]
+    },
+    ios: {
+      unstable_reactLegacyComponentNames: [
+        // list of conponents that needs to be wrapped by the interop layer
+      ]
+    }
   }
 }
